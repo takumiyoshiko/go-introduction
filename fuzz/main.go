4c648893f9e0fb5310d78f6aaf583f6e9f4f4882
takumiyoshiko <takumiyoshiko@gmail.com>
2022-06-04 23:47:39 +0900

    finish fuzzing

--- fuzz/main.go
+++ fuzz/main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
 
-func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1{
-		b[i], b[j] = b[j], b[i]
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is invalid utf8 string")
 	}
-	return string(b)
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1{
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
 }
 
 func main() {
 	input := "The quick brown fox jumps over the lazy dog";
-	rev := Reverse(input)
-	dobuleRev := Reverse(rev)
+	rev, revErr := Reverse(input)
+	dobuleRev, dobuleRevErr := Reverse(rev)
 	fmt.Printf("original: %q\n", input)
-	fmt.Printf("reversed: %q\n", rev)
-	fmt.Printf("reversed again: %q\n", dobuleRev)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", dobuleRev, dobuleRevErr)
 }
--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
-// func TestReverse(f *testing.F) {
-// 	testcases := []string{"Hello, world", " ", "!12345"}
-// 	for _, tc := range testcases {
-// 		f.add(tc)
-// 	}
-// 	f.Fuzz(func)
-// }
-
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
-	}
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
 	for _, tc := range testcases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
-		}
+		f.Add(tc)
 	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
+		}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid utf8 string: %q", rev)
+		}
+	})
 }
+
+// func TestReverse(t *testing.T) {
+// 	testcases := []struct {
+// 		in, want string
+// 	}{
+// 		{"Hello, world", "dlrow ,olleH"},
+// 		{" ", " "},
+// 		{"!12345", "54321!"},
+// 	}
+// 	for _, tc := range testcases {
+// 		rev := Reverse(tc.in)
+// 		if rev != tc.want {
+// 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+// 		}
+// 	}
+// }
